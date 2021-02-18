9c2c57d2727487a5d1b0ced02631dbd829d8b1cd
Craig McKeachie <craigdmckeachie@gmail.com>
2021-02-17 19:41:16 -0500

    03B.07

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Landing from "./Landing";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  // console.error("fail");
-  useEffect(() => {
-    const timer = setTimeout(
-      () => setLoading(previousLoading => !previousLoading),
-      2000
-    );
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Data is: {loading ? "loading" : "loaded"}</Text>
-      <StatusBar style="auto" />
+      <Landing />
     </View>
   );
 }
---
+++ Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Landing() {
+  return (
+    <View>
+      <Text>Dinner and a Movie</Text>
+      <Text>
+        Tap on a film to see the details then pick a date to see showtimes.
+      </Text>
+    </View>
+  );
+}
