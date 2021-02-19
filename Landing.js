ed0824e1dece22951c29f4e4eaf39812167d9437
Craig McKeachie <craigdmckeachie@gmail.com>
2021-02-19 14:28:56 -0500

    04.26

--- Landing.js
+++ Landing.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, Text, Button } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useDispatch } from "react-redux";
 import FilmBrief from "./FilmBrief";
 
-export default function Landing({ films }) {
+export default function Landing({ films, selected_date }) {
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
+  function showDatepicker() {
+    setShow(previousShow => !previousShow);
+  }
+  function onChange(event, selectedDate) {
+    const currentDate = selectedDate || selected_date;
+    setShow(previousShow => !previousShow);
+    dispatch({ type: "SET_SELECTED_DATE", date: currentDate });
+  }
   return (
-    <View>
+    <ScrollView>
       <View>
         <Text>Dinner And a Movie.</Text>
         <Text>Tap a film to see the details. Pick a date to see showtimes</Text>
+        <Button
+          onPress={showDatepicker}
+          title={`Showing times for ${selected_date.toDateString()} `}
+        />
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={selected_date}
+            mode="date"
+            display="default"
+            onChange={onChange}
+          />
+        )}
       </View>
       {films.map(film => {
         return <FilmBrief key={film.id} film={film} />;
       })}
-    </View>
+    </ScrollView>
   );
 }
--- store/reducers.js
+++ store/reducers.js
@@ -2,6 +2,8 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_FILMS_SUCCESS":
       return { ...state, films: [...action.films] };
+    case "SET_SELECTED_DATE":
+      return { ...state, selected_date: action.date };
     default:
       return state;
   }
