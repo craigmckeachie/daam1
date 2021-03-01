165854b87174207c4f1fa6eb9e40deebcca176eb
Craig McKeachie <craigdmckeachie@gmail.com>
2021-03-01 16:16:16 -0500

    08.20

--- ShowingTimes.js
+++ ShowingTimes.js
@@ -2,8 +2,16 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import "./helpers/Date";
 import { theme } from "./theme";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ShowingTimes({ selected_date, showings }) {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  function pickShowingTime(showing) {
+    dispatch({ type: "HIDE_FILM_DETAILS" });
+    navigation.push("PickSeats");
+  }
   return (
     <View style={styles.container}>
       <Text style={{ ...theme.text.subtitle, textAlign: "center" }}>
@@ -11,7 +19,11 @@ export default function ShowingTimes({ selected_date, showings }) {
       </Text>
       <View style={styles.showings}>
         {showings.map(showing => (
-          <Text style={theme.text.normal} key={showing.id}>
+          <Text
+            style={theme.text.normal}
+            key={showing.id}
+            onPress={() => pickShowingTime(showing)}
+          >
             {new Date(showing.showing_time).toShowingTimeString()}
           </Text>
         ))}
