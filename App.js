ba1d218be7368df665863f09bf12253ab15d4b37
Craig McKeachie <craigdmckeachie@gmail.com>
2021-03-01 16:04:25 -0500

    08.10

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -5,6 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Checkout from "./Checkout";
 import Landing from "./Landing";
 import PickSeats from "./PickSeats";
+
+const Nav = createStackNavigator();
 
 export default function App() {
   const state = useSelector(state => state);
@@ -15,12 +19,14 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" hidden={true} />
-      {/* <Landing {...state} /> */}
-      {/* <Checkout /> */}
-      <PickSeats />
-    </View>
+      <View style={styles.container}>
+        <Nav.Navigator>
+          <Nav.Screen name="Home">{() => <Landing {...state} />}</Nav.Screen>
+        </Nav.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
