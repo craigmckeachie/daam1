d5cd0dba87b29fa1123214280be2239c22c9c6f2
Craig McKeachie <craigdmckeachie@gmail.com>
2021-03-02 12:59:40 -0500

    08.38

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
@@ -8,6 +8,15 @@ import Checkout from "./Checkout";
 import Landing from "./Landing";
 import PickSeats from "./PickSeats";
 import Ticket from "./Ticket";
+import { theme } from "./theme";
+
+const appTheme = {
+  ...DefaultTheme,
+  colors: {
+    background: "#fff",
+    card: theme.colors.altLight
+  }
+};
 
 const Nav = createStackNavigator();
 
@@ -20,12 +29,14 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={appTheme}>
       <StatusBar barStyle="light-content" hidden={true} />
       <View style={styles.container}>
         <Nav.Navigator initialRouteName="Home">
           <Nav.Screen name="Home">{() => <Landing {...state} />}</Nav.Screen>
-          <Nav.Screen name="PickSeats">{() => <PickSeats />}</Nav.Screen>
+          <Nav.Screen name="PickSeats" options={{ title: "Pick Seats" }}>
+            {() => <PickSeats />}
+          </Nav.Screen>
           <Nav.Screen name="Checkout">
             {() => <Checkout {...state} />}
           </Nav.Screen>
